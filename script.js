8eb806ca93f76068f3a3548b3d1589d65e79f1c3
l0velyvy <145545979+l0velyvy@users.noreply.github.com>
2023-09-27 14:09:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log (`hello world`)
+
+let num = 1;
+var text = `Hello`; 
+console.log(num);
+let string = `hello`;
+string = `hello dont`;
+string = `hello im multi line string`
+string = `hello number is:  ${num}`
