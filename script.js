d60c71aff376cec13ad501b3417cb4b4cbd959f0
Evelyn Eichenberg <evelyn.eichenberg@tptlive.ee>
2023-10-11 14:11:48 +0300

    kodutoo

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-for(let i=0; i<10; i++){
-    console.log(i);
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
+
+
+
+function fibonacci() {
+    var a = 0, b = 1, count = 0;
+
+    for(var sum = 0; sum < 4000000;){
+
+        sum = a + b;
+
+        a = b;          
+        b = sum;        
+
+        if(sum % 2 == 0){
+            count += sum;
+        }
+    }
+    return count;
 }
 
-let i=1
-while(i<1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000){
-console.log(i)
-i*=2
-}
-
-let object ={
-    name: "emglisch"
-    age: "2"
-    eyes: "bitch"
-}
+console.log(fibonacci());
