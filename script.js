9b54cf257d2e3e9a4bdb88b5c1d300ba30abef53
Evelyn Eichenberg <evelyn.eichenberg@tptlive.ee>
2023-10-18 13:40:58 +0300

    upload

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-var sum = 0;
-for (var x = 0; x < 1000; x++)
-{
-    if (x % 3 === 0 || x % 5 === 0)
-    {
-       sum += x;
-    }
+for(let i=1;i<=200;i++){
+
+    if(i%5===0 && i%7===0){
+        console.log('foobar');
+    } else if(i%5===0){
+        console.log('foo');
+    } else if (i%7===0){
+        console.log('bar');
+    } 
+    console.log(i);
 }
-console.log(sum);
-
-
-
-function fibonacci() {
-    var a = 0, b = 1, count = 0;
-
-    for(var sum = 0; sum < 4000000;){
-
-        sum = a + b;
-
-        a = b;          
-        b = sum;        
-
-        if(sum % 2 == 0){
-            count += sum;
-        }
-    }
-    return count;
-}
-
-console.log(fibonacci());
